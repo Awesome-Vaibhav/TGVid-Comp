12492215e6b7027650cd2d71e75d170c25f94d4d
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2022-09-12 06:56:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ try:
     API_HASH = "6b99e012069f373abbcac581d3cdd6db"
     BOT_TOKEN = "5504367878:AAHAGnkxPdKXCyDAx6atevENv7lzu4qSVEA"
     DEV = 1664850827
-    OWNER = "1144738419 1160615379 5798143340"
+    OWNER = "1144738419 1160615379 5798143340 974552063"
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://graph.org/file/68caa8276fbeff04b1172.jpg")
 except Exception as e:
